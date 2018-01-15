a603e835256a0764f8d261951c871a2a621234db
Vorh <sorcerer12@list.ru>
2018-01-15 21:39:07 +0300

    [ ADD ] hello world

---
+++ src/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?>
+</body>
+</html>
