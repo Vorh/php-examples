348296b61cc65934378d81562d2e33020d6e7a63
Vorh <sorcerer12@list.ru>
2018-01-17 23:31:32 +0300

    [ ADD ] html and css

--- src/task_manager/index.php
+++ src/task_manager/index.php
@@ -3,10 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" href="./style.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
 </head>
 <body>
+<div id="root">
+    <div class="panel">
+        <div class="caption">TODOS</div>
+        <div class="button">New Todo</div>
+    </div>
 
 
+    <div class="todos">
+        <div class="todo">
+            <div class="caption-t"> Create new script
+            </div>
+            <div class="date-t">Due: Aug 1, 2015</div>
+            <div class="btn-t-complete"><i class="fas fa-check"></i></div>
+            <div class="btn-t-delete"><i class="fas fa-trash"></i></div>
+        </div>
+        <div class="todo">Two todo</div>
+        <div class="todo">Three todo</div>
 
+    </div>
+
+</div>
 </body>
 </html>
