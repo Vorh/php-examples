2718aa7e9abb1467dff8bcb150e11b213d4fd9c7
Vorh <sorcerer12@list.ru>
2018-01-30 01:33:48 +0300

    [ ADD ]  template

---
+++ src/site/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <title>CSGO-EASY - Открывайте кейсы CS:GO с выгодой!</title>
+    <meta name="description"
+          content="Оружейные кейсы CS:GO по 89 рублей. Теперь кейсы можно открывать дешевле, чем в Steam">
+    <meta name="keywords" content="case csgo, кейсы cs go, кейс кс го">
+    <link rel="shortcut icon" type="image/x-icon" href="http://www.sovercase/static/images/csicon.png">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link href="static/css/style.css" rel="stylesheet">
+    <link href="static/css/styles.css" rel="stylesheet">
+    <link href="static/css/bootstrap.css" rel="stylesheet">
+    <link href="static/css/jquery.css" rel="stylesheet" media="screen">
+
+    <link href="static/css/css.css" rel="stylesheet" type="text/css">
+    <link href="static/css/css_002.css" rel="stylesheet" type="text/css">
+
+    <script type="text/javascript" async="" src="static/js/watch.js"></script>
+    <script src="static/js/watch(1).js" async="" type="text/javascript"></script>
+    <script type="text/javascript" src="static/js/share.js"></script>
+    <script type="text/javascript" src="static/js/openapi.js"></script>
+
+    <script type="text/javascript" src="static/js/jquery.js"></script>
+    <script type="text/javascript" src="static/js/socket.js"></script>
+
+    <script type="text/javascript" src="static/js/bootstrap.js"></script>
+    <script type="text/javascript" src="static/js/jquery_003.js"></script>
+    <script type="text/javascript" src="static/js/jquery_002.js"></script>
+    <script type="text/javascript" src="static/js/scrolling-nav.js"></script>
+    <script type="text/javascript" src="static/js/cases.js"></script>
+    <script type="text/javascript" src="static/js/server.js"></script>
+    <script type="text/javascript" src="static/js/payment.js"></script>
+    <link rel="shortcut icon" href="http://localhost:8081/favicon.gif" type="image/gif">
+
+    <style class="firebugResetStyles" type="text/css" charset="utf-8"></style>
+</head>
+
+<body style="overflow: visible; margin-right: 0px;">
+<div class="wrapper" style="width: 1125px; padding-top: 5px;">
+    <?php include 'template/header.php'  ?>
+    <div class="bgcsgo"></div>
+
+    <?php include 'template/last_winners.php'  ?>
+
+    <?php include 'template/items.php'  ?>
+
+
+    <div class="clearfix"></div>
+    <div id="recent_wins2"></div>
+    <div id="reviews">
+        <hr class="style-two">
+    </div>
+    <div class="clearfix"></div>
+
+    <?php  include 'template/bottonm.php' ?>
+
+</div>
+<div id="ulogin_receiver_container"
+     style="margin: 0px; padding: 0px; outline: none; border: none; border-radius: 0px; cursor: default; float: none; position: relative; display: none; width: 0px; height: 0px; left: 0px; top: 0px; box-sizing: content-box;">
+    <iframe name="easyXDM_default1581_provider" id="easyXDM_default1581_provider" frameborder="0"
+            src="static/css/stats.html"
+            style="margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; position: absolute; left: 0px; top: 0px; overflow: hidden; width: 100%; height: 100%;"></iframe>
+</div> <!-- wrapper -->
+
+
+<?php include 'template/buy_modal.php' ?>
+
+
+<?php include 'template/condition.php' ?>
+
+<?php include 'template/metric.php' ?>
+
+
+</body>
+<div></div>
+</html>
---
+++ src/site/template/bottom.php
@@ -0,0 +1,20 @@
+<div style="width: 700px; margin: 0 auto; " class="vksw">
+    <div class="stone" style="font-size: 13px;
+        line-height: 15px;
+        color: #eaeaea;
+        padding: 10px;
+        margin-left: -10px;
+        margin-right: -10px;
+        margin-bottom: 5px;
+        text-align: center;">
+        <span style="color: #EFDA08;">ВНИМАНИЕ!</span> Steam ввел новые ограничения, поэтому сейчас <br>действуют
+        новые условия покупки. <a style="color: #EFDA08; cursor: pointer; text-decoration: underline;"
+                                  data-modal="#rules">Ознакомиться с новыми условиями</a>
+        <div style="padding-top: 5px;">
+            Перед тем, как задавать вопросы в комментариях, почитайте <a
+                style="color: #EFDA08; cursor: pointer; text-decoration: underline;" data-modal="#siteFAQ"
+                href="http://localhost:8081/select/aj_random_thing#">"Помощь по сайту"</a>
+        </div>
+
+    </div>
+</div>
